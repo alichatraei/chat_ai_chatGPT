97d6dae66ce729c1a2411310ca1fda74ce2de6d7
Alichatraei <avida.chatraee2001@yahoo.com>
2023-02-03 16:59:41 +0330

    fix: fix bugs

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ const onSubmit = async (e) => {
     const messageDiv = document.querySelector(`#${uniqueId}`)
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://server-utnm.onrender.com', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json"
